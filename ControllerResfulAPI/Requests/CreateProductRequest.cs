2047b34107a2cd3ef140084c45f257e493c4c317
Mourad Dellali <mourad.dellali@ensia.edu.dz>
2026-03-14 16:48:13 +0100

    creating Requests

---
+++ ControllerResfulAPI/Requests/CreateProductRequest.cs
@@ -0,0 +1,7 @@
+namespace ControllerResfulAPI.Requests;
+
+public class CreateProductRequest
+{
+    public string? Name {get;set;}
+    public decimal Price {get;set;}
+}
--- ControllerResfulAPI/obj/Debug/net10.0/ControllerResfulAPI.AssemblyInfo.cs
+++ ControllerResfulAPI/obj/Debug/net10.0/ControllerResfulAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControllerResfulAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d87de13763b5bd0008b899260f7cb064802db33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39accc0a42def6cfa4de0fa93fa953ea597d7854")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControllerResfulAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControllerResfulAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
